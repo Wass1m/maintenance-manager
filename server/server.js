35bb8da5a794e9bea02d4883234bc99da21f2449
MACBOOK <fm_ghernaout@esi.dz>
2021-12-25 20:50:32 +0100

    configuring server for production

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const { initApp } = require("./controllers/Users");
+var cors = require("cors");
 
 // parser les jsons
 app.use(express.json());
+app.use(cors({ credentials: true, origin: true }));
 
 app.use(express.static(__dirname + "/public"));
 
@@ -27,7 +29,7 @@ var PORT = 5000 || process.env.PORT;
 
 // serveur
 db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`SERVER RUNNING ON PORT ${PORT}`);
   });
   initApp();
