a326f3f2a6b68b1def686ff0717ba191a08c4f32
MACBOOK <fm_ghernaout@esi.dz>
2021-12-27 01:18:19 +0100

    final configurations

--- client/src/components/responsable/ressources/PageQR.js
+++ client/src/components/responsable/ressources/PageQR.js
@@ -32,7 +32,7 @@ const PageQR = ({
           <QRCode
             size="150"
             className="qr-image"
-            value={`http://10.201.70.228:3000/signaler/${ressourceID}`}
+            value={`${REACT_APP_URL}signaler/${ressourceID}`}
           />
 
           <div className="qr-infos">
@@ -40,7 +40,7 @@ const PageQR = ({
               <h1>Flashez-moi</h1>
               <h4>Pour rapporter un problème avec ce materiel</h4>
             </div>
-            <div className="qr-url">{`http://10.201.70.228:3000/signaler/${ressourceID}`}</div>
+            <div className="qr-url">{`${REACT_APP_URL}signaler/${ressourceID}`}</div>
           </div>
         </div>
       </div>
