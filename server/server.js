cab65db0fe61416a27037ce79731c9abd8e6d26a
MACBOOK <fm_ghernaout@esi.dz>
2021-12-28 13:01:20 +0100

    fixing ips

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express();
 const { initApp } = require("./controllers/Users");
 var cors = require("cors");
 
+var os = require("os");
+var networkInterfaces = os.networkInterfaces();
+console.log(networkInterfaces);
+
 // parser les jsons
 app.use(express.json());
 app.use(cors({ credentials: true, origin: true }));
@@ -29,7 +33,7 @@ var PORT = 5000 || process.env.PORT;
 
 // serveur
 db.sequelize.sync().then(() => {
-  app.listen(PORT, "0.0.0.0", () => {
+  app.listen(PORT, () => {
     console.log(`SERVER RUNNING ON PORT ${PORT}`);
   });
   initApp();
